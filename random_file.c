3ce85770bd9132e43877fba0bade84329e6bf1d2
Atilade01 <hatilade@gmail.com>
2022-11-12 05:42:55 -0800

    Task 0 - Beautiful code

--- random_file.c
+++ random_file.c
@@ -2,26 +2,21 @@
 
 /**
  * main - Addition and substraction of two numbers
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int num1, num2, num3, num4;
+	int sum = num1 + num2;
+	int subtract = num3 - num4;
 
 	printf("Please enter two numbers: ");
-
 	scanf("%d %d", &num1, &num2);
-
-	int sum = num1 + num2;
-
 	printf("This is the sum of the numbers inputted: %d\n", sum);
 	printf("Do you want to continue?\n");
 	printf("If yes enter two numbers: ");
 	scanf("%d %d", &num3, &num4);
-
-	int subs = num3 - num4;
-
 	printf("This is the result of the substraction: %d\n", subs);
+	
 	return (0);
 }
